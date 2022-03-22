8b281394fd150c49669f75611f61bd6e45b5d83f
Tomáš Sabol <tomassabol2@gmail.com>
2022-03-22 15:53:34 +0100

    updated sql script

--- scripts/CreateTables.sql
+++ scripts/CreateTables.sql
@@ -1,21 +1,12 @@
-drop table if exists Storages;
-drop table if exists StorageLines;
 drop table if exists Products;
 drop table if exists ProductTypes;
+drop table if exists Storages;
+drop table if exists StorageLines;
 drop table if exists Suppliers;
 drop table if exists Customers;
-
-create table Products(
-    Id int identity(1,1) primary key,
-    Name varchar(100),
-    Price smallmoney,
-    inStock int,
-    foreign key (inStock) references StorageLines(Quantity),
-    ProductTypeId int,
-    foreign key (ProductTypeId) references ProductTypes(Id),
-    SupplierId int,
-    foreign key (SupplierId) references Supplier(Id)
-);
+drop table if exists CustomerTypes;
+drop table if exists Orders;
+drop table if exists OrderProducts;
 
 create table Suppliers(
     Id int identity(1,1) primary key,
@@ -31,19 +22,16 @@ create table ProductTypes(
     TypeName varchar(100)
 );
 
-create table StorageLines(
-    Id int identity(1,1),
-    ProductId int,
-    foreign key (ProductId) references Products(Id),
-    Quantity int,
-    StorageId int,
-    foreign key (StorageId) references Storages(Id)
-);
-
 create table Storages(
     Id int identity(1,1) primary key,
     Name varchar(100),
     Address varchar(100)
+);
+
+create table CustomerTypes(
+    Id int identity(1,1) primary key,
+    Name varchar(100),
+    Discount int
 );
 
 create table Customers(
@@ -57,12 +45,6 @@ create table Customers(
     foreign key (CustomerTypeId) references CustomerTypes(Id)
 );
 
-create table CustomerTypes(
-    Id int identity(1,1) primary key,
-    Name varchar(100),
-    Discount int
-);
-
 create table Orders(
     Id int identity(1,1) primary key,
     [Date] date,
@@ -71,10 +53,31 @@ create table Orders(
     foreign key (CustomerId) references Customers(Id),
 );
 
+create table Products(
+    Id int identity(1,1) primary key,
+    Name varchar(100),
+    Price smallmoney,
+    inStock int,
+    foreign key (inStock) references StorageLines(Quantity),
+    ProductTypeId int,
+    foreign key (ProductTypeId) references ProductTypes(Id),
+    SupplierId int,
+    foreign key (SupplierId) references Suppliers(Id)
+);
+
 create table OrderProducts(
     ProductId int,
     foreign key (ProductId) references Products(Id),
     Quantity int,
     OrderId int,
     foreign key (OrderId) references Orders(Id)
+);
+
+create table StorageLines(
+    Id int identity(1,1),
+    ProductId int,
+    foreign key (ProductId) references Products(Id),
+    Quantity int,
+    StorageId int,
+    foreign key (StorageId) references Storages(Id)
 );
