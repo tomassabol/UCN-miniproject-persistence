4e07156f2791d367a9696848cbdbe85162b45c93
ttommmy <93518302+ttommmy@users.noreply.github.com>
2022-03-24 11:32:35 +0100

    Customer menu completed

--- src/view/CustomerMenu.java
+++ src/view/CustomerMenu.java
@@ -3,17 +3,29 @@ package view;
 import java.sql.SQLException;
 
 import controller.CustomerController;
+import model.Customer;
 
 public class CustomerMenu {
     
+	/**
+	 * Fields for class CustomerMenu
+	 */
     private CustomerController customerCtrl;
     private Input input;
 
+    /**
+     * Constructor for class CustomerMenu
+     * @throws SQLException
+     */
     public CustomerMenu() throws SQLException {
         customerCtrl = new CustomerController();
         input = new Input();
     }
 
+    /**
+     * Prints out the menu and asks for a user input
+     * @throws SQLException
+     */
     public void run() throws SQLException {
         boolean conTinUe = true;
         ListChoice<Integer> menu = new ListChoice<>();
@@ -23,6 +35,7 @@ public class CustomerMenu {
         menu.addOption("View customer", 3);
         menu.addOption("Update customer", 4);
         menu.addOption("Delete customer", 5);
+        //menu.addOption("Customer type menu", 6);
         System.out.println();
 
         while(conTinUe) {
@@ -30,23 +43,23 @@ public class CustomerMenu {
             choice = menu.input("Customer menu - Choose option:", false);
             switch(choice) {
                 case 1: {
-
+                	createCustomer();
                     break;
                 }
                 case 2: {
-
+                	listAllCustomer();
                     break;
                 }
                 case 3: {
-
+                	findCustomerById();
                     break;
                 }
                 case 4: {
-
+                	updateCustomer();
                     break;
                 }
                 case 5: {
-
+                	deleteCustomer();
                     break;
                 }
                 case 0: {
@@ -54,10 +67,63 @@ public class CustomerMenu {
                     break;
                 }
                 default: {
-
                     break;
                 }
             }
         }
+    }
+    
+    public void createCustomer() throws SQLException {
+    	clearLines();
+    	
+    	String name = input.stringInput("Enter customer's name: ");
+    	String address = input.stringInput("Enter customer's address: ");
+    	String city = input.stringInput("Enter customer's city: ");
+    	String phoneNumber = input.stringInput("Enter customer's phone number: ");
+    	String email = input.stringInput("Enter customer's email: ");
+    	
+    	customerCtrl.createCustomer(name, address, city, phoneNumber, email);
+    }
+    
+    public void listAllCustomer() throws SQLException {
+    	clearLines();
+    	
+    	for(Customer customer : customerCtrl.findAll()) {
+    		System.out.println(customer.toString());
+    	}
+    	
+    }
+    
+    public Customer findCustomerById() throws SQLException {
+    	listAllCustomer();
+    	int id = input.integerInput("Enter customer's id: ");
+    	Customer customer = customerCtrl.findCustomerById(id);
+    	System.out.println(customer.toString());
+    	return customer;
+    }
+    
+    public void updateCustomer() throws SQLException {
+    	listAllCustomer();
+    	int id = input.integerInput("Enter customer's id: ");
+    	Customer customer = customerCtrl.findCustomerById(id);
+    	
+    	String name = input.stringInput("Enter customer's name: ");
+    	String address = input.stringInput("Enter customer's address: ");
+    	String city = input.stringInput("Enter customer's city: ");
+    	String phoneNumber = input.stringInput("Enter customer's phone number: ");
+    	String email = input.stringInput("Enter customer's email: ");
+    	
+    	customerCtrl.updateCustomer(customer, name, address, city, phoneNumber, email);
+    }
+    
+    public void deleteCustomer() throws SQLException {
+    	Customer customer = findCustomerById();
+    	customerCtrl.deleteCustomer(customer);
+    }
+    
+    public void clearLines() {
+    	for(int i = 0; i<10; i++) {
+    		System.out.println();
+    	}
     }
 }
