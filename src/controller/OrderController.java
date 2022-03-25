505f9dcab0f0efc48f87ae81a34040f238b5f3c6
Tomáš Sabol <tomassabol2@gmail.com>
2022-03-25 10:36:14 +0100

    added createOredrLine method

--- src/controller/OrderController.java
+++ src/controller/OrderController.java
@@ -119,4 +119,8 @@ public class OrderController {
     public void finishOrder(Order order) throws SQLException {
         orderDBIF.createOrder(order);
     }
+
+    public void createOrderLine(Order order, OrderLine orderLine) throws SQLException {
+        orderDBIF.createOrderLine(order, orderLine);
+    }
 }
